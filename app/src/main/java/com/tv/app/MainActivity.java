16ad25d82aa1e7b57ab47efaeb640abf504a9e72
charlienycusa-a11y <charlienycusa@icloud.com>
2026-01-21 21:42:53 +0300

    Update MainActivity.java

--- app/src/main/java/com/tv/app/MainActivity.java
+++ app/src/main/java/com/tv/app/MainActivity.java
@@ -1,12 +1,39 @@
 package com.tv.app;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.leanback.app.BrowseSupportFragment;
+import androidx.leanback.widget.ArrayObjectAdapter;
+import androidx.leanback.widget.HeaderItem;
+import androidx.leanback.widget.ListRow;
+import androidx.leanback.widget.ListRowPresenter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // This is a blank screen for your TV app
+        setContentView(R.layout.activity_main);
+
+        BrowseSupportFragment browseFragment = (BrowseSupportFragment) 
+                getSupportFragmentManager().findFragmentById(R.id.main_browse_fragment);
+
+        if (browseFragment != null) {
+            // This sets the title in the top right, like Plex
+            browseFragment.setTitle("IPTV PLAYER");
+            // This sets the color of the sidebar
+            browseFragment.setBrandColor(getResources().getColor(android.R.color.black));
+            
+            loadRows(browseFragment);
+        }
+    }
+
+    private void loadRows(BrowseSupportFragment fragment) {
+        ArrayObjectAdapter rowsAdapter = new ArrayObjectAdapter(new ListRowPresenter());
+        
+        // This creates your first "Plex" category
+        HeaderItem header = new HeaderItem(0, "LIVE TV");
+        rowsAdapter.add(new ListRow(header, new ArrayObjectAdapter())); // We will fill this next
+
+        fragment.setAdapter(rowsAdapter);
     }
 }
